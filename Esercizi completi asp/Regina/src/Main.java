d9e93777b773e3651de3ecb11f1e85cdcf4b77ce
frumorn <andreabrescia002@gmail.com>
2024-04-23 12:46:58 +0200

    messo cella

--- Esercizi completi asp/Regina/src/Main.java
+++ Esercizi completi asp/Regina/src/Main.java
@@ -1,5 +1,39 @@
+import it.unical.mat.embasp.base.Handler;
+import it.unical.mat.embasp.base.InputProgram;
+import it.unical.mat.embasp.base.Output;
+import it.unical.mat.embasp.languages.IllegalAnnotationException;
+import it.unical.mat.embasp.languages.ObjectNotValidException;
+import it.unical.mat.embasp.languages.asp.ASPInputProgram;
+import it.unical.mat.embasp.languages.asp.ASPMapper;
+import it.unical.mat.embasp.languages.asp.AnswerSet;
+import it.unical.mat.embasp.languages.asp.AnswerSets;
+import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
+import it.unical.mat.embasp.specializations.dlv.desktop.DLVDesktopService;
+import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
+
+
 public class Main {
+
+    private static Handler handler;
+
+    private int[][] matrice = {
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0}
+    };
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        handler = new DesktopHandler(new DLV2DesktopService("lib/dlv2"));
+
+
+
     }
 }
